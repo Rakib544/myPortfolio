ffac66187fa4dcad75563a17c470ffad730c48fc
Rakib544 <md.rakib10122003@gmail.com>
2021-05-03 11:00:19 +0600

    new commit done

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDownload } from '@fortawesome/free-solid-svg-icons'
